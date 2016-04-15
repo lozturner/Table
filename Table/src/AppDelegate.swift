f3b49d95e243d8ec4fe90e25df4ff8dac815cc4d
Eonist <unicornhive@gmail.com>
2016-04-15 11:40:31 +0200

    Files modified: 1
    Modified 1 file:Table/src/AppDelegate.swift

--- Table/src/AppDelegate.swift
+++ Table/src/AppDelegate.swift
@@ -6,7 +6,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        StyleManager.addStyle("Window{fill:white;corner-radius:4px;}")
+        //StyleManager.addStyle("Window{fill:white;corner-radius:4px;}")
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/basic/window/window.css")
         win = AppWin(300,400)
         let app:NSApplication = aNotification.object as! NSApplication/*grab the app instance from the notification*/
         app.windows[0].close()/*close the initial non-optional default window*/
