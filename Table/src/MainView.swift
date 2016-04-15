2d873c17457f1d1e423242acc588f997632ae526
Eonist <unicornhive@gmail.com>
2016-04-15 09:04:11 +0200

    Files modified: 1
    Modified 1 file:Table/src/MainView.swift

--- Table/src/MainView.swift
+++ Table/src/MainView.swift
@@ -1,8 +1,30 @@
 import Foundation
 
 class MainView:CustomView {
-    var container:Section?
+    var container:Container!
+    var fileWatcher:FileWatcher?
     override func resolveSkin() {
         super.resolveSkin()
+        createTable()
+        container = addSubView(Container(1000,800,self,"main"))
+    }
+    /**
+     *
+     */
+    func createTable(){
+        let url:String = "~/Desktop/ElCapitan/other/gradientpanel/gradientpanel.css"
+        StyleManager.addStylesByURL(url,true)
+        
+        fileWatcher = FileWatcher([url.tildePath])
+        fileWatcher!.event = { event in
+            //Swift.print(self)
+            Swift.print(event.description)
+            if(event.fileChange && event.path == url.tildePath) {
+                StyleManager.addStylesByURL(url,true)
+                ElementModifier.refreshSkin(self)
+                ElementModifier.floatChildren(self)
+            }
+        }
+        fileWatcher!.start()
     }
 }
