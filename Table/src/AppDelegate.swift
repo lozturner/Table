347b766bacec4625045e628ab41b5420b8b75538
Eonist <unicornhive@gmail.com>
2016-04-15 08:59:53 +0200

    New files added: 1
    Added 1 file:Table/src/AppDelegate.swift

---
+++ Table/src/AppDelegate.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    weak var window: NSWindow!
+    var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        StyleManager.addStyle("Window{fill:white;corner-radius:4px;}")
+        win = AppWin(300,400)
+        let app:NSApplication = aNotification.object as! NSApplication/*grab the app instance from the notification*/
+        app.windows[0].close()/*close the initial non-optional default window*/
+    }
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+}
