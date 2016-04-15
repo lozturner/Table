f8e65a5bcfa2f568bbe4e1cefc21c7a18cd81ab1
Eonist <unicornhive@gmail.com>
2016-04-15 09:32:19 +0200

    Files modified: 1
    Modified 1 file:Table/src/MainView.swift

--- Table/src/MainView.swift
+++ Table/src/MainView.swift
@@ -39,5 +39,10 @@ class MainView:CustomView {
         //TableModifier.selectRow(table,1);
         
         // :TODO: TableParser.rowAt(0) should return an array with each the text from each row item
+        
+        //continue here: add TitleBar Icons css
+        //Get rid of gradient in table 
+        
+        
     }
 }
